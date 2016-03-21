676e811131ac6265435dbb186523f0fb8fa50056
furukawa0120 <ntbyk439@ybb.ne.jp>
2016-03-21 23:13:35 +0900

    bootstrap Sample Application Update
    サンプルプログラム更新
    　・画像ファイルのアップロード・表示
    　・メニューバー、サイドバーからの画面遷移
    　・各編集項目レイアウト微調整（テキスト長など）

--- bootstrap_test/app/controllers/profiles_controller.rb
+++ bootstrap_test/app/controllers/profiles_controller.rb
@@ -69,6 +69,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:name, :age, :charge, :comment)
+      params.require(:profile).permit(:name, :age, :charge, :comment, :image, :image_cache, :remove_image)
     end
 end
--- bootstrap_test/db/schema.rb
+++ bootstrap_test/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207081412) do
+ActiveRecord::Schema.define(version: 20160320091312) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160207081412) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
